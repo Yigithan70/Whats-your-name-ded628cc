1f33577212ff47b7c3bad8b1b25923e700bd125e
Yigithan70 <60740198+Yigithan70@users.noreply.github.com>
2020-06-11 10:39:07 +0200

    Add files via upload

---
+++ name.php
@@ -0,0 +1,10 @@
+<?php
+
+
+        $line1 = readline("Wat is je voornaam? ");
+        $line2 = readline("Wat is je achternaam? ");
+        $line3 = readline("Jouw naam is: $line1  $line2");
+        readline_add_history($line);
+
+
+?>
